070c1b0a63e4ceecad79bc1aad955bef2e261fa0
Katiuska Alicea <katiuskaalicea@gmail.com>
2022-05-24 02:28:50 -0700

    added values to the MainTheme

--- appropertyinvestors/src/styles/MainTheme.js
+++ appropertyinvestors/src/styles/MainTheme.js
@@ -1,2 +1,27 @@
 import { createTheme } from "@mui/material/styles";
-export const MainTheme = createTheme();
+export const MainTheme = createTheme({
+  palette: {
+    primary: {
+      main: "#28292e",
+      contrastText: "#eaebea",
+    },
+    secondary: {
+      main: "#eaebea",
+      contrastText: "#28292f",
+    },
+  },
+
+  typography: {
+    fontSize: 16,
+  },
+
+  breakpoints: {
+    values: {
+      xs: 0,
+      sm: 600,
+      md: 900,
+      lg: 1200,
+      xl: 1500,
+    },
+  },
+});
