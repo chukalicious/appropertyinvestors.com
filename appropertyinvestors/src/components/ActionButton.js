aa5bb6e257c62e200e6f351b4417e4e162bc2505
Katiuska Alicea <katiuskaalicea@gmail.com>
2022-05-24 02:17:05 -0700

    created hidden action button on navbar

---
+++ appropertyinvestors/src/components/ActionButton.js
@@ -0,0 +1,11 @@
+import Button from "@mui/material/Button";
+
+const ActionButton = () => {
+  return (
+    <Button sx={{ color: "secondary", display: "none" }} variant="contained">
+      contact us
+    </Button>
+  );
+};
+
+export default ActionButton;
