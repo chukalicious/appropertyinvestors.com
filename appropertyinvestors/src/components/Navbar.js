35dfd063b5cef0eb800208f70fd141931caebde4
Katiuska Alicea <katiuskaalicea@gmail.com>
2022-05-24 10:38:59 -0700

    added hidden action button to the navbar component, text to the nav bar and the position prop that will prevent adjecent components to hide behind the navbar

--- appropertyinvestors/src/components/Navbar.js
+++ appropertyinvestors/src/components/Navbar.js
@@ -3,22 +3,29 @@ import Typography from "@mui/material/Typography";
 import AppBar from "@mui/material/AppBar";
 import Menu from "./Menu";
 import Toolbar from "@mui/material/Toolbar";
-import { headerStyles, titleBox, menuBox, barStyles } from "../styles/Header";
+import ActionButton from "./ActionButton";
+import {
+  headerStyles,
+  titleBox,
+  menuBox,
+  barStyles,
+  buttonWrapper,
+} from "../styles/Header";
 
 const Navbar = () => {
   return (
     <Box sx={{ ...headerStyles }}>
-      {/* this is the header containing box */}
-      <AppBar sx={{ ...barStyles }}>
+      <AppBar position="sticky" sx={{ ...barStyles }}>
         <Toolbar>
           <Box sx={{ ...titleBox }}>
-            {/* this box will contain the title text */}
-            <Typography>A&P Property Investors</Typography>{" "}
+            <Typography variant="body1" component="h1">
+              A&P Property Investors
+            </Typography>{" "}
           </Box>
           <Box sx={{ ...menuBox }}>
             <Menu />
-            {/* This box will contain the menu */}
           </Box>
+          <ActionButton />
         </Toolbar>
       </AppBar>{" "}
     </Box>
