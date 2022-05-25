59fe3702fb265f0b424a95e80beca58bf0086567
Katiuska Alicea <katiuskaalicea@gmail.com>
2022-05-24 22:09:38 -0700

    added menu bar component, nav menu component, refactored generic boxes in the navbar, added styles for a menu icon box in order to be able to hide it

--- appropertyinvestors/src/styles/Header.js
+++ appropertyinvestors/src/styles/Header.js
@@ -9,12 +9,13 @@ export const HeaderBox = styled("div")(({ theme }) => ({
 
 export const StyledBar = styled("div")(({ theme }) => ({
   width: "100%",
+  border: "purple 2px solid",
 }));
 
 export const TitleBox = styled("div")(({ theme }) => ({
   width: "60%",
   display: "flex",
-  border: "blue 2px solid",
+  // border: "blue 2px solid",
   [theme.breakpoints.up("md")]: {
     width: "40%",
   },
@@ -23,12 +24,23 @@ export const TitleBox = styled("div")(({ theme }) => ({
 export const MenuBox = styled("div")(({ theme }) => ({
   display: "flex",
   width: "40%",
-  border: "yellow solid 2px",
+  // border: "yellow solid 2px",
   [theme.breakpoints.up("md")]: {
     width: "60%",
+    border: "blue 2px solid",
   },
 }));
 
-export const buttonWrapper = {
-  border: "lightpink solid 1px",
-};
+export const MenuIcon = styled("div")(({ theme }) => ({
+  [theme.breakpoints.down("md")]: {
+    display: "flex",
+  },
+
+  [theme.breakpoints.up("md")]: {
+    display: "none",
+  },
+}));
+
+// export const buttonWrapper = {
+//   border: "lightpink solid 1px",
+// };
